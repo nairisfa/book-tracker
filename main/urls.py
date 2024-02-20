3a8d79d94071986f8884e0ab2a75478cc8772534
nairisfa <nailakrisnazulfa@gmail.com>
2024-02-20 09:26:25 +0700

    tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from main.views import show_main
-from main.views import show_main, create_book, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register
+from main.views import logout_user
+from main.views import show_main, create_book, show_xml, show_json, show_xml_by_id, show_json_by_id, login_user
 
 app_name = 'main'
 
 urlpatterns= [
+    path('logout/', logout_user, name='logout'),
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('json/', show_json, name='show_json'),
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import BookForm
 from main.models import Book
@@ -5,13 +12,17 @@ from django.http import HttpResponse
 from django.core import serializers
 
 # Create your views here.
+
+@login_required(login_url='/login')
+
 def show_main(request):
-    books = Book.objects.all()
+    books = Book.objects.filter(user=request.user)
 
     context = {
-        'name': 'Pak Bepe',
+        'name': request.user.username,
         'class': 'PBP A',
-        'books': books
+        'books': books,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -20,7 +31,9 @@ def create_book(request):
     form = BookForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        book = form.save(commit=False)
+        book.user = request.user
+        book.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -41,3 +54,36 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Book.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
